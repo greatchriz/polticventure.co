8afc87dc725f73b1e98cd584baa1f0ea57f4cd92
greatchriz <89561544+greatchriz@users.noreply.github.com>
2023-08-04 21:15:05 +0100

    Update joint-deposit-list.php

--- joint-deposit-list.php
+++ joint-deposit-list.php
@@ -402,13 +402,9 @@
         </nav>
 
 
-        <main class="main-content w-full px-[var(--margin-x)] pb-8 mt-4">
-
-
-            <div class="grid grid-cols-1 gap-4 sm:gap-5 lg:gap-6">
-
-
-                <div class="card rounded-lg px-4 py-4 sm:px-5">
+        <main class="main-content w-full px-[var(--margin-x)] pb-8">
+            <div class="mt-4 grid grid-cols-12 gap-4 sm:mt-5 sm:gap-5 lg:mt-6 lg:gap-6">
+                <div class="card col-span-12 px-4 pb-5 sm:px-5">
                     <label class="inline-flex items-center space-x-2">
                         <input class="form-radio is-basic h-5 w-5 rounded-full border-slate-400/70 checked:border-primary checked:bg-primary hover:border-primary focus:border-primary dark:border-navy-400 dark:checked:border-accent dark:checked:bg-accent dark:hover:border-accent dark:focus:border-accent" name="h_id" type="radio" value="1" checked="">
                         <p>Investor</p>
@@ -488,3 +484,7 @@
 </body>
 
 </html>
+
+<main class="main-content w-full px-[var(--margin-x)] pb-8">
+    <div class="mt-4 grid grid-cols-12 gap-4 sm:mt-5 sm:gap-5 lg:mt-6 lg:gap-6">
+        <div class="card col-span-12 px-4 pb-5 sm:px-5">
