b4d0b81bc55ff17945dc4f0678fd3697aeba10ad
greatchriz <89561544+greatchriz@users.noreply.github.com>
2023-08-04 17:51:59 +0100

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,44 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+
+    // Replace with your desired email address
+    $to = "your_email@example.com";
+    $subject = "Form Submission from $name";
+    $body = "Name: $name\nEmail: $email\nMessage: $message";
+
+    // Additional headers
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+
+    // Sending email
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Thank you for your message!";
+    } else {
+        echo "Oops! Something went wrong. Please try again later.";
+    }
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Contact Form</title>
+</head>
+<body>
+    <h1>Contact Form</h1>
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+        <label for="name">Name:</label>
+        <input type="text" name="name" id="name" required>
+        <br>
+        <label for="email">Email:</label>
+        <input type="email" name="email" id="email" required>
+        <br>
+        <label for="message">Message:</label>
+        <textarea name="message" id="message" required></textarea>
+        <br>
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
