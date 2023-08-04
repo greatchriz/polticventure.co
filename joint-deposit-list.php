ef627b258706e36daf1f70e2bed9a4a72dc9a1aa
greatchriz <89561544+greatchriz@users.noreply.github.com>
2023-08-04 21:43:04 +0100

    Update joint-deposit-list.php

--- joint-deposit-list.php
+++ joint-deposit-list.php
@@ -403,78 +403,186 @@
 
 
         <main class="main-content w-full px-[var(--margin-x)] pb-8">
-            <div class="flex items-center space-x-4 py-5 lg:py-6">
-                <ul class="flex flex-wrap items-center space-x-2">
-                    <li class="flex items-center space-x-2">
-                        <a class="rounded-lg border border-slate-200 px-1.5 py-1 leading-none text-primary transition-colors hover:text-primary-focus dark:border-navy-500 dark:text-accent-light dark:hover:text-accent" href="?a=account">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4.5 w-4.5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path>
-                            </svg>
-                        </a>
-                        <svg x-ignore="" xmlns="http://www.w3.org/2000/svg" class="h-3.5 w-3.5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path>
+            <div class="mt-5 flex items-center justify-between">
+                <h3 class="text-lg font-medium text-slate-700 line-clamp-1 dark:text-navy-50">
+                    Workspaces Overview
+                </h3>
+                <div class="flex">
+                    <button class="btn h-8 w-8 rounded-full p-0 hover:bg-slate-300/20 focus:bg-slate-300/20 active:bg-slate-300/25 dark:hover:bg-navy-300/20 dark:focus:bg-navy-300/20 dark:active:bg-navy-300/25">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1.5">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"></path>
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
                         </svg>
-                    </li>
-                    <li class="flex items-center space-x-2">
-                        <a class="flex items-center space-x-1.5 rounded-lg border border-slate-200 py-1 px-1.5 leading-none text-primary transition-colors hover:text-primary-focus dark:border-navy-500 dark:text-accent-light dark:hover:text-accent" href="?a=deposit_list">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4.5 w-4.5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z"></path>
-                            </svg>
-                            <span>Deposit List</span>
-                        </a>
-                        <svg x-ignore="" xmlns="http://www.w3.org/2000/svg" class="h-3.5 w-3.5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path>
+                    </button>
+                    <button class="btn h-8 w-8 rounded-full p-0 hover:bg-slate-300/20 focus:bg-slate-300/20 active:bg-slate-300/25 dark:hover:bg-navy-300/20 dark:focus:bg-navy-300/20 dark:active:bg-navy-300/25">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1.5">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
                         </svg>
-                    </li>
-                </ul>
+                    </button>
+                </div>
             </div>
-            <div class="mt-4 grid grid-cols-12 gap-4 sm:mt-5 sm:gap-5 lg:mt-6 lg:gap-6">
-                <div class="card col-span-12 p-4 pb-5 sm:px-5">
-                    
-                    <div class="mt-4 h-px  bg-slate-200 dark:bg-navy-500"></div>
 
-
-                    <div class="pt-2 mt-2">
-
-                        <div class="flex justify-between items-center">
-                            <p class="font-semibold text-slate-600">Rate </p>
-                            <p>1.50% / daily</p>
+            <div class="mt-4 sm:mt-5 lg:mt-6">
+                <div class="flex items-center justify-between">
+                    <h2 class="text-base font-medium tracking-wide text-slate-700 line-clamp-1 dark:text-navy-100">
+                        Monitoring Workspaces
+                    </h2>
+                    <div class="flex">
+                        <div class="flex items-center" x-data="{isInputActive:false}">
+                            <label class="block">
+                                <input x-effect="isInputActive === true &amp;&amp; $nextTick(() => { $el.focus()});" :class="isInputActive ? 'w-32 lg:w-48' : 'w-0'" class="form-input bg-transparent px-1 text-right transition-all duration-100 placeholder:text-slate-500 dark:placeholder:text-navy-200 w-0" placeholder="Search here..." type="text">
+                            </label>
+                            <button @click="isInputActive = !isInputActive" class="btn h-8 w-8 rounded-full p-0 hover:bg-slate-300/20 focus:bg-slate-300/20 active:bg-slate-300/25 dark:hover:bg-navy-300/20 dark:focus:bg-navy-300/20 dark:active:bg-navy-300/25">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-4.5 w-4.5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+                                </svg>
+                            </button>
                         </div>
-
-                        <div class="my-4 h-px  bg-slate-200 dark:bg-navy-500"></div>
-
-
-                        <div class="flex justify-between items-center">
-                            <p class="font-semibold text-slate-600">Minimum -Maximum Inv. Amount</p>
-                            <p>$200.00 - $4999.00</p>
+                        <div x-data="usePopper({placement:'bottom-end',offset:4})" @click.outside="isShowPopper &amp;&amp; (isShowPopper = false)" class="inline-flex">
+                            <button x-ref="popperRef" @click="isShowPopper = !isShowPopper" class="btn h-8 w-8 rounded-full p-0 hover:bg-slate-300/20 focus:bg-slate-300/20 active:bg-slate-300/25 dark:hover:bg-navy-300/20 dark:focus:bg-navy-300/20 dark:active:bg-navy-300/25">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-4.5 w-4.5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"></path>
+                                </svg>
+                            </button>
+                            <div x-ref="popperRoot" class="popper-root" :class="isShowPopper &amp;&amp; 'show'" style="position: fixed; inset: 0px 0px auto auto; margin: 0px; transform: translate(-16px, 172px);" data-popper-placement="bottom-end">
+                                <div class="popper-box rounded-md border border-slate-150 bg-white py-1.5 font-inter dark:border-navy-500 dark:bg-navy-700">
+                                    <ul>
+                                        <li>
+                                            <a href="#" class="flex h-8 items-center px-3 pr-8 font-medium tracking-wide outline-none transition-all hover:bg-slate-100 hover:text-slate-800 focus:bg-slate-100 focus:text-slate-800 dark:hover:bg-navy-600 dark:hover:text-navy-100 dark:focus:bg-navy-600 dark:focus:text-navy-100">Action</a>
+                                        </li>
+                                        <li>
+                                            <a href="#" class="flex h-8 items-center px-3 pr-8 font-medium tracking-wide outline-none transition-all hover:bg-slate-100 hover:text-slate-800 focus:bg-slate-100 focus:text-slate-800 dark:hover:bg-navy-600 dark:hover:text-navy-100 dark:focus:bg-navy-600 dark:focus:text-navy-100">Another Action</a>
+                                        </li>
+                                        <li>
+                                            <a href="#" class="flex h-8 items-center px-3 pr-8 font-medium tracking-wide outline-none transition-all hover:bg-slate-100 hover:text-slate-800 focus:bg-slate-100 focus:text-slate-800 dark:hover:bg-navy-600 dark:hover:text-navy-100 dark:focus:bg-navy-600 dark:focus:text-navy-100">Something else</a>
+                                        </li>
+                                    </ul>
+                                    <div class="my-1 h-px bg-slate-150 dark:bg-navy-500"></div>
+                                    <ul>
+                                        <li>
+                                            <a href="#" class="flex h-8 items-center px-3 pr-8 font-medium tracking-wide outline-none transition-all hover:bg-slate-100 hover:text-slate-800 focus:bg-slate-100 focus:text-slate-800 dark:hover:bg-navy-600 dark:hover:text-navy-100 dark:focus:bg-navy-600 dark:focus:text-navy-100">Separated Link</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
                         </div>
-
-                        <div class="my-4 h-px  bg-slate-200 dark:bg-navy-500"></div>
-
-
-                        <div class="flex justify-between items-center">
-                            <p class="font-semibold text-slate-600">R.O.I.</p>
-                            <p>1.50%</p>
-                        </div>
-                        <div class="my-4 h-px  bg-slate-200 dark:bg-navy-500"></div>
-
-                        <div class="flex justify-between items-center">
-                            <p class="font-semibold text-slate-600">Duration</p>
-                            <p>7 DAYS</p>
-                        </div>
-
-                        <div class="my-4 h-px  bg-slate-200 dark:bg-navy-500"></div>
-
-                        <div class="flex justify-between items-center">
-                            <p class="font-semibold text-slate-600">Referral Bonus</p>
-                            <p>10% on First Deposit</p>
-                        </div>
-
                     </div>
+                </div>
+                <div class="mt-3 grid grid-cols-1 gap-4 sm:gap-5 lg:gap-6">
+                    <div class="card space-y-6 p-4 sm:px-5">
+                        <div class="flex items-center justify-between">
+                            <div>
+                                <p class="text-lg font-semibold uppercase text-primary dark:text-accent-light">
+                                    Id: 5988745
+                                </p>
+                            </div>
+                            <div x-data="usePopper({placement:'bottom-end',offset:4})" @click.outside="isShowPopper &amp;&amp; (isShowPopper = false)" class="inline-flex">
+                                <button x-ref="popperRef" @click="isShowPopper = !isShowPopper" class="btn -mr-1.5 h-8 w-8 rounded-full p-0 hover:bg-slate-300/20 focus:bg-slate-300/20 active:bg-slate-300/25 dark:hover:bg-navy-300/20 dark:focus:bg-navy-300/20 dark:active:bg-navy-300/25">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path>
+                                    </svg>
+                                </button>
+
+                                <div x-ref="popperRoot" class="popper-root" :class="isShowPopper &amp;&amp; 'show'" style="position: fixed; inset: 0px 0px auto auto; margin: 0px; transform: translate(0px, 232px);" data-popper-placement="bottom-end">
+                                    <div class="popper-box rounded-md border border-slate-150 bg-white py-1.5 font-inter dark:border-navy-500 dark:bg-navy-700">
+                                        <ul>
+                                            <li>
+                                                <a href="#" class="flex h-8 items-center px-3 pr-8 font-medium tracking-wide outline-none transition-all hover:bg-slate-100 hover:text-slate-800 focus:bg-slate-100 focus:text-slate-800 dark:hover:bg-navy-600 dark:hover:text-navy-100 dark:focus:bg-navy-600 dark:focus:text-navy-100">Action</a>
+                                            </li>
+                                            <li>
+                                                <a href="#" class="flex h-8 items-center px-3 pr-8 font-medium tracking-wide outline-none transition-all hover:bg-slate-100 hover:text-slate-800 focus:bg-slate-100 focus:text-slate-800 dark:hover:bg-navy-600 dark:hover:text-navy-100 dark:focus:bg-navy-600 dark:focus:text-navy-100">Another Action</a>
+                                            </li>
+                                            <li>
+                                                <a href="#" class="flex h-8 items-center px-3 pr-8 font-medium tracking-wide outline-none transition-all hover:bg-slate-100 hover:text-slate-800 focus:bg-slate-100 focus:text-slate-800 dark:hover:bg-navy-600 dark:hover:text-navy-100 dark:focus:bg-navy-600 dark:focus:text-navy-100">Something else</a>
+                                            </li>
+                                        </ul>
+                                        <div class="my-1 h-px bg-slate-150 dark:bg-navy-500"></div>
+                                        <ul>
+                                            <li>
+                                                <a href="#" class="flex h-8 items-center px-3 pr-8 font-medium tracking-wide outline-none transition-all hover:bg-slate-100 hover:text-slate-800 focus:bg-slate-100 focus:text-slate-800 dark:hover:bg-navy-600 dark:hover:text-navy-100 dark:focus:bg-navy-600 dark:focus:text-navy-100">Separated Link</a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <p class="text-xs uppercase">Current Status</p>
+                            <p class="text-base font-medium text-success">Up for 7 hours</p>
+                        </div>
+                        <div class="flex grow justify-between space-x-2">
+                            <div>
+                                <p class="text-xs+ text-slate-400 dark:text-navy-300">
+                                    Total Uptime
+                                </p>
+                                <p class="text-lg font-semibold text-slate-700 dark:text-navy-100">
+                                    96.4%
+                                </p>
+                            </div>
+                            <div>
+                                <p class="text-xs+ text-slate-400 dark:text-navy-300">
+                                    Response
+                                </p>
+                                <p class="text-lg font-semibold text-slate-700 dark:text-navy-100">
+                                    3.4 s
+                                </p>
+                            </div>
+                            <div>
+                                <p class="text-xs+ text-slate-400 dark:text-navy-300">
+                                    Memory
+                                </p>
+                                <p class="text-lg font-semibold text-slate-700 dark:text-navy-100">
+                                    14 GB
+                                </p>
+                            </div>
+                        </div>
+                        <div class="space-y-3">
+                            <div class="flex justify-between">
+                                <p class="text-xs uppercase text-slate-400 dark:text-navy-300">
+                                    Country
+                                </p>
+                                <p class="text-xs uppercase text-slate-400 dark:text-navy-300">
+                                    traffic
+                                </p>
+                            </div>
+                            <div class="flex items-center justify-between">
+                                <div class="flex items-center space-x-2">
+                                    <img class="h-6 w-6" src="images/flags/usa-round.svg" alt="flag">
+                                    <p class="font-medium text-slate-700 dark:text-navy-100">
+                                        Unated States
+                                    </p>
+                                </div>
+                                <div class="font-inter">
+                                    <span class="text-right font-medium text-slate-700 dark:text-navy-100">4.5 GB</span>
+                                    <span class="text-xs">/8 GB</span>
+                                </div>
+                            </div>
+                            <div class="flex items-center justify-between">
+                                <div class="flex items-center space-x-2">
+                                    <img class="h-6 w-6" src="images/flags/russia-round.svg" alt="flag">
+                                    <p class="font-medium text-slate-700 dark:text-navy-100">
+                                        Russia
+                                    </p>
+                                </div>
+                                <div class="font-inter">
+                                    <span class="text-right font-medium text-slate-700 dark:text-navy-100">6.22 GB</span>
+                                    <span class="text-xs">/12 GB</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="flex flex-wrap space-x-2">
+                            <div class="badge bg-info/10 text-info dark:bg-info/15">
+                                195.161.66.25
+                            </div>
+                            <div class="badge bg-info/10 text-info dark:bg-info/15">
+                                215.122.127.155
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
 
             </div>
-
         </main>
     </div>
     <!-- 
